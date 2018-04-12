72228727b8384387fe65b03cf69b39f4306dee83
tugcem <tugcemyalcin@gmail.com>
2018-04-12 16:17:51 +1000

    Extends middleware to expire and run with given rate limit parameters.

--- app/middleware/rate_limiter.rb
+++ app/middleware/rate_limiter.rb
@@ -1,3 +1,6 @@
+RATE_LIMIT             = 100
+RATE_LIMIT_EXPIRE_TIME = 3600
+
 module Middleware
   class RateLimiter
     def initialize app
@@ -5,13 +8,42 @@ module Middleware
     end
 
     def call env
-      increment_request_count env
-      @app.call env
+      client_ip env['REMOTE_ADDR']
+      increment_request_count > RATE_LIMIT ? too_many_requests : @app.call(env)
     end
 
-    def increment_request_count env
-      client_ip = env['REMOTE_ADDR']
-      REDIS.get(client_ip) ? REDIS.incr(client_ip) : REDIS.set(client_ip, 1)
+    private
+
+    def client_ip ip=nil
+      @client_ip ||= ip
+    end
+
+    def request_count
+      REDIS.get(client_ip)&.to_i
+    end
+
+    def initialize_request_count
+      REDIS.set(client_ip, 0)
+      REDIS.EXPIRE(client_ip, RATE_LIMIT_EXPIRE_TIME)
+    end
+
+    def increment_request_count
+      initialize_request_count unless request_count
+      REDIS.incr(client_ip)&.to_i
+    end
+
+    def time_to_reset_counter
+      REDIS.ttl(client_ip)
+    end
+
+    def too_many_requests
+      [
+        429,
+        {},
+        [
+          "Rate limit exceeded. Try again in #{time_to_reset_counter} seconds"
+        ]
+      ]
     end
   end
 end
